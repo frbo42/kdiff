08715fe519c1190827bd27a1b2b7630a2b7b6c54
frank <frank.bommeli@mimacom.com>
2019-11-15 15:04:22 -0700

    test doesn't fail anymore

--- src/test/kotlin/org/fb/kdiff/HelloTest.kt
+++ src/test/kotlin/org/fb/kdiff/HelloTest.kt
@@ -17,11 +17,6 @@ class HelloTest {
         assertTrue( execute(t, t))
     }
 
-    @Test
-    internal fun `dummy failing test`() {
-        fail<String>("I failed")
-    }
-
     private fun execute(a: Boolean, b: Boolean): Boolean = !(!a && !b)
     fun execute2(a: Boolean, b: Boolean): Boolean = !(!a || !b)
 
